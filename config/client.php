04c1a8fe816fafee2f6457b3b6392f42e117b4e1
Marcel Stadelmann <m@marceli.to>
2025-05-12 13:43:52 +0200

    wip

---
+++ config/client.php
@@ -0,0 +1,5 @@
+<?php
+return [
+  'base_url' => env('APP_URL'),
+  'meta_description' => 'Im Herzen von Zollikon entsteht mit dem Projekt magnolia. ein Wohntraum der besonderen Art. Vielleicht Ihr neues Zuhause?'
+];
