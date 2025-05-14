762ecd3e93ee91478caab82da571c917fb7d0235
Marcel Stadelmann <m@marceli.to>
2025-05-14 08:56:27 +0200

    wip

--- resources/js/modules/maps.js
+++ resources/js/modules/maps.js
@@ -21,7 +21,7 @@ const initMap = () => {
         coordinates: [8.576795782295077,47.345146618634324]
       },
       properties: {
-        title: 'Magnolia Rothfluhstrasse',
+        title: 'Magnolia Rothfluhstrasse Zollikon',
         description: 'Pfäffikon ZH'
       }
     }]
